0ad74f22230467fcb59c2e8ea0d03ba8187077fb
Musa ayoola shittu <109434707+Mifimn@users.noreply.github.com>
2026-01-19 22:04:23 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://mifimnpay.vercel.app',
+  siteUrl: 'https://mifimnpay.com.ng',
   generateRobotsTxt: true, // This creates the robots.txt file automatically
   sitemapSize: 7000,
   // List pages you DON'T want Google to show in search results
